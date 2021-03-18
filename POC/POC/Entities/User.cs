b60c344c5275436f1fee619cf1e7640e0cd14029
Sakshi Gupta <50147978+Sakshi-7699@users.noreply.github.com>
2021-03-18 22:37:07 +0530

    Add files via upload

---
+++ POC/POC/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace POC.Entities
+{
+    public class User
+    {
+        public string fname { get; set; }
+        public string lname { get; set; }      
+
+        public int user_id { get; set; }
+
+        public int phone { get; set; }
+
+        public string password { get; set; }
+    }
+}

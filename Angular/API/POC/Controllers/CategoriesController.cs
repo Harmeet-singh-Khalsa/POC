8167bdf8c5c2bbba0447280ad60d56fff1df99e7
Sakshi-7699 <std9.341999@gmail.com>
2021-04-03 19:03:29 +0530

    components mapped,nav bar added and mapping done

---
+++ Angular/API/POC/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using POC.DTOs;
+using POC.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace POC.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+
+        private readonly ILogger<CartController> logger;
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public CategoriesController(
+            ApplicationDbContext context,
+            IMapper mapper)
+        {
+          
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+
+        [HttpGet("{Id:int}", Name = "getCategory")]
+            public async Task<ActionResult<ProductsDTO>> Get(int Id)
+            {
+                var products
+                = await context.Products.FirstOrDefaultAsync(x => x.category_id == Id);
+
+                if (products == null)
+                {
+                    return NotFound();
+                }
+
+                var productDTO = mapper.Map<ProductsDTO>(products);
+
+                return productDTO;
+            }
+       
+    }
+}
---
+++ Angular/API/POC/Entities/Categories.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace POC.Entities
+{
+    public class Categories
+    {
+        [Key]
+        public int category_id { get; set; }
+        public string category_name { get; set; }
+       
+        
+    }
+}
+
